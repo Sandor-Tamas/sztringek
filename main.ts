2918146e472f43920399dbefe2e4f33ad2dafe29
Sandor-Tamas <70057013+Sandor-Tamas@users.noreply.github.com>
2020-08-23 14:56:55 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
-let list: string[] = []
+let list: number[] = []
 let betu = 65
-for (let index = 0; index <= 4; index++) {
-    list.push(String.fromCharCode(betu))
-    basic.showString("" + (list[index]))
+let szoveg: string[] = []
+let i = 0
+for (let index = 0; index <= 8; index++) {
+    list[index * 2] = betu
+    list[index * 2 + 1] = betu + 32
     betu = betu + 1
+}
+for (let value of list) {
+    szoveg[i] = String.fromCharCode(value)
+    basic.showString("" + (szoveg[i]))
+    i = i + 1
 }
 basic.forever(function () {
 	
